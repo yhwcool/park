e28bd4574864290dc1dfb22224469ab5a349c61e
yhwcool <710761418@qq.com>
2018-11-25 17:08:32 +0800

    commit

--- WebContent/jsp/dist/js/loadIframe.js
+++
@@ -1,9 +0,0 @@
-function setIframeHeight(iframe) {
-	if (iframe) {
-			var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
-			if (iframeWin.document.body) {
-				
-				iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
-			}
-	}
-};
